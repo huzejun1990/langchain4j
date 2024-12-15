aef05b45ee92aeb8787c1185cb7465f74789267e
huzejun <961639130@qq.com>
2024-12-15 17:29:47 +0800

    AIService快速构建完成智能体 2024-12-15 17:27:57+ModelUtil微调

--- src/main/java/com/dream/tools/ModelUtil.java
+++ src/main/java/com/dream/tools/ModelUtil.java
@@ -12,14 +12,14 @@ public class ModelUtil
 {
     public static final String API_KEY_ZHIPU="2423212";
 
-    public static final String API_KEY_OPENAI="sk-DbMfA18yhwgH（afdaadsfafasfdasdf）LHPx32F48c03E1C44416B0B81d3096F1297e";
-    // sk-DbMfA18yhwgHLHPx32(asfdasfxw)F48c03E1C44416B0B81d3096F1297e
-    // sk-Qxt1e040220a75c18e3a2(safsafda)193f6b5cb0d8fb7ff3uxxI1
-//    public static final String API_KEY_OPENAI="sk-Qxt1e040(sfaasdfas)220a75c18e3a2193f6b5cb0d8fb7ff3uxxI1"; //other user
-    public static final String BASE_URI_OPENAI="https://api.x(asfasfdsafasfdas)ty.app/v1";   //https://ap(safasfda)i.xty.app
-    // https://api.gp(asdfsa)tsapi.net/v1;
-//    public static final String BASE_URI_OPENAI="https://api.g(sdfassf)ptsapi.net/v1";
-    public static ChatLanguageModel getOpenAiModel(){
+    public static final String API_KEY_OPENAI="sk-DbMfA18yhwgHLHPx32F4（aslasfdas8sfaas）8c03E1C44416B0B81d3096F1297e";
+    // sk-DbMfA18yhwgHLHPx（aslasfdas8sfaas）32F48c03E1C44416B0B81d3096F1297e
+    // sk-Qxt1e040220a75c18e3a2193（aslasfdas8sfaas）f6b5cb0d8fb7ff3uxxI1
+//    public static final String API_KEY_OPENAI="sk-Qxt1e0402（aslasfdas8sfaas）20a75c18e3a2193f6b5cb0d8fb7ff3uxxI1"; //other user
+    public static final String BASE_URI_OPENAI="https://api.xt（aslasfdas8sfaasy.app/v1";   //https://api.（aslasfdas8sfaasxty.app
+    // https://api.gp（aslasfdas8sfaastsapi.net/v1;
+//    public static final String BASE_URI_OPENAI="https://api.gp（aslasfdas8sfaastsapi.net/v1";
+    public static ChatLanguageModel getOpenAIModel(){
         return OpenAiChatModel.builder()
 //                .apiKey(API_KEY_OPENAI)
 //                .baseUrl(BASE_URI_OPENAI)
